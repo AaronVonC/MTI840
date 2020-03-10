257f2d707a9629a3f4295a6138a176be5b63c6d8
AaronVonC <45091803+AaronVonC@users.noreply.github.com>
2020-03-10 16:41:39 -0400

    Update devices.py

--- devices.py
+++ devices.py
@@ -2,25 +2,29 @@ import socket
 import os
 from time import sleep
 import tqdm
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(13, GPIO.IN, GPIO.PUD_DOWN)
 SEPARATOR="<SEPARATOR>"
 BUFFER_SIZE = 4096
 host = "10.196.113.18"
 port = 8080
-
-os.system("fswebcam -r 1280x720 --no-banner image/image.jpg")
-filename = "image/image.jpg"
-filesize = os.path.getsize(filename)
-s=socket.socket()
-print(f"[+] Connecting to {host}:{port}")
-s.connect((host, port))
-print("[+] Connected.")
-s.send(f"{filename}{SEPARATOR}{filesize}".encode())
-progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
-with open(filename, "rb") as f:
-    for _ in progress:
-        bytes_read = f.read(BUFFER_SIZE)
-        if not bytes_read:
-            break
-        s.sendall(bytes_read)
-        progress.update(len(bytes_read))
-s.close()
+while(1):
+    if GPIO.input(13)==0:
+        os.system("fswebcam -r 1280x720 --no-banner image/image.jpg")
+        filename = "image/image.jpg"
+        filesize = os.path.getsize(filename)
+        s=socket.socket()
+        print(f"[+] Connecting to {host}:{port}")
+        s.connect((host, port))
+        print("[+] Connected.")
+        s.send(f"{filename}{SEPARATOR}{filesize}".encode())
+        progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+        with open(filename, "rb") as f:
+            for _ in progress:
+                bytes_read = f.read(BUFFER_SIZE)
+                if not bytes_read:
+                    break
+                s.sendall(bytes_read)
+                progress.update(len(bytes_read))
+        s.close()
